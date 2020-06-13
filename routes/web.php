23861f9d7bb7177977e101cf5efcba79773b0ad2
Preshy-Jones <adedibuprecious@gmail.com>
2020-06-13 04:35:51 +0100

    initial import

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 */
 
-// Route::get('/', function () {
+Route::get('/', function () {
     
-//     return view('welcome');
-// });
+    return view('welcome');
+});
 
 // Route::get('/hello', function () {
     
